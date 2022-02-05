88541357a146be1d27764ba121a34c18c827caeb
Yongsen Luo <2612758L@student.gla.ac.uk>
2022-02-05 20:11:41 +0000

    Commit chapter three exercises

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,12 @@
+from pydoc import html
+from re import A
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango say hey there partner!")
+    return HttpResponse("Rango says hey there partner! \
+        <br/> <a href='/rango/about/'>about</a>")
+
+def about(request):
+    return HttpResponse("Rango say here is the about page\
+        <br/> <a href='/rango/'> index</a>")
